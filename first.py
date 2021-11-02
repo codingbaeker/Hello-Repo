330f43c80c638567439eece6dd9df1fe7ba7713c
codingbaeker <jannik.himmelsbach@gmx.de>
2021-11-02 15:56:38 -0600

    next commit

--- first.py
+++ first.py
@@ -1,1 +1,30 @@
-print ('hello world')
+# var = 'mundo'
+# num = 1234
+# num2 = 678
+
+# flotante = 123.4
+# flotante2 = 564.2
+# carac = 'c'
+# carac2 = 'd'
+
+# bol = True
+# bol2 = False
+# bol3 = None
+
+# print(var)
+# print(num + num2)
+# print(flotante + flotante2)
+
+# print(carac  + carac2)
+# print(var + carac)
+
+# print(bol or bol2)
+# print(bol and bol2)
+# print(not bol)
+# print(not bol2)
+
+
+for i in range(101):
+    if i%2 == 0:
+        print(i)
+
